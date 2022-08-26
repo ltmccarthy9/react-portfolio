b5f7b78473d51637a53e70852f45d3c22bd432d8
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-25 20:51:01 -0500

    Portfolio section

---
+++ src/components/Portfolio.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+export default function Portfolio() {
+    const [currentSection, setCurrentSection] = useState('About');
+
+    const renderSection = () => {
+        if (currentSection === 'About') {
+            return <About />;
+        }
+        if (currentSection === 'Projects') {
+            return <Projects />
+        }
+        if (currentSection === 'Contact') {
+            return <Contact />
+        }
+        return <Resume />;
+    };
+
+    const handleSectionChange = (section) => setCurrentSection(section);
+
+    return (
+        <div>
+            <NavTabs currentSection={currentSection} handleSectionChange={handleSectionChange} />
+            {renderSection()}
+        </div>
+    )
+}

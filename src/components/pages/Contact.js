41e4312f1766a869dce444f1f316f626800ffb6f
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-25 21:35:35 -0500

    Contact Page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>
+          Email:
+          LinkedIn:
+          Github:
+        </p>
+      </div>
+    );
+  }
+  

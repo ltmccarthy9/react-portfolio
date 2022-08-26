b0b38ad55ba5b90bc54b8cf08b551744b56885ad
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-25 21:33:47 -0500

    projects section

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,16 @@
+
+export default function Projects() {
+    return (
+        <div>
+        <div className="projectCard">
+            Project 1
+        </div>
+        <div className="projectCard">
+            Project 2
+        </div>
+        <div className="projectCard">
+            Project 3
+        </div>
+        </div>
+    )
+}

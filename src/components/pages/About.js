dce5bdeb2a0d83624134d92cec1351f0c2a56b44
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-25 21:29:01 -0500

    about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Hello! My name is Liam McCarthy.  I am a full stack developer out of Chicago, IL.
+                This is my portfolio, you can navigate to different sections in the navigation bar above.
+            </p>
+        </div>
+    );
+}
